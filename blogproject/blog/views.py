f9da6e13ffe9c2fc07e11232bbbb76e431aad3db
tomus1857 <tomus1857.2001@o2.pl>
2025-05-28 15:09:03 +0200

    added new home template, new home view and new home style

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -6,3 +6,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def home (request):
+    posts = Post.objects.all().order_by('-date')[:3]
+    return render(request, 'blog/home.html', {'posts':posts})
